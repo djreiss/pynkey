e27fa5d09943ab7a446b1ffd38d11b0fa4c03215
dreiss.isb <dreiss.isb@gmail.com>
2014-05-27 22:10:10 -0700

    adding sequence functions, not complete yet

--- init.py
+++ init.py
@@ -7,6 +7,8 @@ import os
 from Bio import SeqIO
 import gzip
 
+import sequence as seq
+
 print 'init'
 
 def pynkey_init(organism, k_clust, ratios_file):
@@ -56,7 +58,7 @@ def pynkey_init(organism, k_clust, ratios_file):
     # ## TODO: Need to add 0's for k-mers that are NOT in the sequences.
     # ##   Use generate_all_kmers() for that.
     # ## TODO: Need to include vague IUPAC symbols better
-    # all_seqs = get_sequences(anno["sysName"].data,anno,genome_seqs,true,op_table,distance_search,false); 
+    all_seqs = seq.get_sequences(anno["sysName"].data,anno,genome_seqs,true,op_table,distance_search,false); 
     # all_seqs = all_seqs[ find(all_seqs[:,1].!=""), : ] 
     # all_seqs = filter_sequences( all_seqs, distance_search )
 
@@ -104,12 +106,12 @@ def load_ratios(rats_file):
     ## Although ratios.values does the same thing ;)
     ## Note e.g. ratios.mean(1) is about 5.5x slower than ratios.values.mean(1)
 
-    good_rows = (x==NA).apply(sum, axis=1) < x.shape[1]/2
+    good_rows = (x==NA).apply(np.sum, axis=1) < x.shape[1]/2
     x = x[good_rows]
     print x.shape
 
     ##for i=1:size(x,1) x[i,:] = stdize_vector(x[i,:]); end ##(x[i,:]-nanmean(x[i,:]))/nansd(x[i,:]); end
-    f = lambda x: (x-np.mean(x.dropna())) / np.std(x.dropna())
+    f = lambda x: (x-x.dropna().mean()) / x.dropna().std()
     x = x.apply(f, axis=1)
     print x.ix[:, :6].head()
     return x
@@ -121,10 +123,10 @@ def load_genome(organism):
     genome_file = './' + organism + '/' + org_files[ np.array( [f.startswith('genome.') for f in org_files] ) ][ 0 ]
     print genome_file
 
-    ##handle = gzip.open('./' + organism + '/' + genome_file)
-    ##genome_seqs = SeqIO.to_dict(SeqIO.parse(handle, "fasta")) ## allows >1 sequence too but the files only have one seq so
-    ##handle.close()
-    genome_seqs = SeqIO.read(gzip.open(genome_file), "fasta") ## this is a python dict
+    handle = gzip.open(genome_file)
+    genome_seqs = SeqIO.to_dict(SeqIO.parse(handle, "fasta")) ## allows >1 sequence too but the files only have one seq so
+    handle.close()
+    ##genome_seqs = SeqIO.read(gzip.open(genome_file), "fasta") ## this is a python dict
     ## get the biosequence via: genome_seqs.values()[0].seq
 
     print genome_seqs
