004309069bb3207bfdd1bf7508b9621199dcdf36
dreiss.isb <dreiss.isb@gmail.com>
2014-06-01 11:07:48 -0700

    slight speedup in getBgCounts and implemented include_revComp option

--- sequence.py
+++ sequence.py
@@ -42,6 +42,10 @@ def get_sequences( genes, anno=None, ##=globals()['anno'],
 
     seqs = {}
     ind = 0
+
+    ## TBD: use dataframe indexes rather than np.where() to speed up lookups.
+    ##ot1 = op_table.copy(); ot1.set_index('SysName1', drop=False, inplace=True)
+    ##ot2 = op_table.copy(); ot2.set_index('SysName2', drop=False, inplace=True)
     for gene in genes:
         upstream_gene = gene
         if gene not in anno.index: ##anno_ind == 0 ##! any(anno["sysName"].data .== gene) 
@@ -53,7 +57,7 @@ def get_sequences( genes, anno=None, ##=globals()['anno'],
             strand = gene_anno['strand']
             upLabel = 'SysName1' if strand == '+' else 'SysName2'
             currLabel = 'SysName2' if strand == '+' else 'SysName1'
-            row = np.where( op_table[currLabel] == gene )[0]
+            row = np.where( op_table[currLabel] == upstream_gene )[0]
             while len(row) > 0 and op_table.ix[ row[0], "pOp" ] >= 0.8:
                 upstream_gene = op_table.ix[ row, upLabel ].values[0]
                 row = np.where( op_table[currLabel] == upstream_gene )[0]
@@ -136,7 +140,7 @@ def readFastaDNA( fname ):
 
 def writeFasta( seqs, fname ): ## Assumes seqs in DataFrame format of get_sequences()
     tmp = [ SeqRecord(Seq(seqs.seq.values[i], IUPAC.ExtendedIUPACDNA()), id=seqs.gene.values[i], 
-                      name=seqs.gene.values[i], description=seqs.gene.values[i]) for i in range(len(seqs)) ]
+                      name=seqs.gene.values[i], description=seqs.gene.values[i]) for i in xrange(len(seqs)) ]
     handle = open(fname, 'w')
     SeqIO.write(tmp, handle, "fasta")
     handle.close()
@@ -154,46 +158,46 @@ def generateAllKmers( length ):
     return all_combos.values
 
 ## this is non-overlapping count! 
-def getBgCounts_FAST_INACCURATE( seqs, order=3, include_revComp=True ):
-    d = {}
-    for ord in range(order+1): ## get counts for 1,2,3,...order
-        print ord
-        all_combos = generateAllKmers( ord+1 )
-        for i in range( len(seqs) ):
-            seq = None
-            if type(seqs) == np.ndarray:
-                seq = Seq(seqs[i], IUPAC.ExtendedIUPACDNA())  ## seqs is a column.values from a DataFrame 
-            elif type(seqs) == dict: 
-                seq = seqs.values()[i].seq ## otherwise assumed to be a dict of Bio.Seqs (e.g. genome_seqs)
-            seq_rev = seq.reverse_complement()
-            for ss in all_combos:
-                if ss not in d.keys():
-                    d[ss] = 0
-                d[ss] += seq.count(ss)
-                d[ss] += seq_rev.count(ss)
-    return d
+# def getBgCounts_FAST_INACCURATE( seqs, order=3, include_revComp=True ):
+#     d = {}
+#     for ord in range(order+1): ## get counts for 1,2,3,...order
+#         print ord
+#         all_combos = generateAllKmers( ord+1 )
+#         for i in xrange( len(seqs) ):
+#             seq = None
+#             if type(seqs) == np.ndarray:
+#                 seq = Seq(seqs[i], IUPAC.ExtendedIUPACDNA())  ## seqs is a column.values from a DataFrame 
+#             elif type(seqs) == dict: 
+#                 seq = seqs.values()[i].seq ## otherwise assumed to be a dict of Bio.Seqs (e.g. genome_seqs)
+#             seq_rev = seq.reverse_complement()
+#             for ss in all_combos:
+#                 if ss not in d.keys():
+#                     d[ss] = 0
+#                 d[ss] += seq.count(ss)
+#                 d[ss] += seq_rev.count(ss)
+#     return d
 
-## Use regex instead to get overlapping counts... see:
-## https://stackoverflow.com/questions/2970520/string-count-with-overlapping-occurances
-def getBgCounts_SLOW_ACCURATE( seqs, order=3, include_revComp=True ):
-    import re
-    d = {}
-    for ord in range(order+1): ## get counts for 1,2,3,...order
-        print ord
-        all_combos = generateAllKmers( ord+1 )
-        for i in range( len(seqs) ):
-            seq = None
-            if type(seqs) == np.ndarray:
-                seq = Seq(seqs[i], IUPAC.ExtendedIUPACDNA())  ## seqs is a column.values from a DataFrame 
-            elif type(seqs) == dict: 
-                seq = seqs.values()[i].seq ## otherwise assumed to be a dict of Bio.Seqs (e.g. genome_seqs)
-            seq_rev = Seq(seq, IUPAC.ExtendedIUPACDNA()).reverse_complement()
-            for ss in all_combos:
-                if ss not in d.keys():
-                    d[ss] = 0
-                d[ss] += len(re.findall('(?='+ss+')', seq))
-                d[ss] += len(re.findall('(?='+ss+')', seq_rev)) 
-    return d
+# ## Use regex instead to get overlapping counts... see:
+# ## https://stackoverflow.com/questions/2970520/string-count-with-overlapping-occurances
+# def getBgCounts_SLOW_ACCURATE( seqs, order=3, include_revComp=True ):
+#     import re
+#     d = {}
+#     for ord in range(order+1): ## get counts for 1,2,3,...order
+#         print ord
+#         all_combos = generateAllKmers( ord+1 )
+#         for i in xrange( len(seqs) ):
+#             seq = None
+#             if type(seqs) == np.ndarray:
+#                 seq = Seq(seqs[i], IUPAC.ExtendedIUPACDNA())  ## seqs is a column.values from a DataFrame 
+#             elif type(seqs) == dict: 
+#                 seq = seqs.values()[i].seq ## otherwise assumed to be a dict of Bio.Seqs (e.g. genome_seqs)
+#             seq_rev = Seq(seq, IUPAC.ExtendedIUPACDNA()).reverse_complement()
+#             for ss in all_combos:
+#                 if ss not in d.keys():
+#                     d[ss] = 0
+#                 d[ss] += len(re.findall('(?='+ss+')', seq))
+#                 d[ss] += len(re.findall('(?='+ss+')', seq_rev)) 
+#     return d
 
 ## This also gets overlapping counts by iteration and counting, is 5.5x faster than the regex method
 ## Note it can use a lot of memory - 2x the size of the biggest input seq (which will be a lot if it's a genome!)
@@ -202,20 +206,27 @@ def getBgCounts_FAST_ACCURATE_BIGMEMORY( seqs, order=3, include_revComp=True ):
     d = {}
     for ord in range(order+1): ## get counts for 1,2,3,...order
         print ord
-        all_combos = generateAllKmers( ord+1 )
+        ord1 = ord+1
+        all_combos = generateAllKmers( ord1 )
         for ss in all_combos:
             d[ss] = 0
-        for i in range( len(seqs) ):
+        for i in xrange( len(seqs) ):
             seq = None
             if type(seqs) == np.ndarray:
                 seq = seqs[i]
             elif type(seqs) == dict: 
                 seq = seqs.values()[i].seq.tostring()
-            seq_rev = reverse_complement(seq)
-            c1 = Counter([seq[i:(i+ord+1)] for i in range(len(seq)-ord)])
-            c2 = Counter([seq_rev[i:(i+ord+1)] for i in range(len(seq_rev)-ord)])
+            seq_rev = reverse_complement(seq) if include_revComp else ''
+            if ord == 0:
+                c1 = Counter([seq[i] for i in xrange(len(seq)-ord)]) ## xrange makes it ~10% faster and uses less memory
+                if include_revComp:
+                    c2 = Counter([seq_rev[i] for i in xrange(len(seq_rev)-ord)])
+            else:
+                c1 = Counter([seq[i:(i+ord1)] for i in xrange(len(seq)-ord)])
+                if include_revComp:
+                    c2 = Counter([seq_rev[i:(i+ord1)] for i in xrange(len(seq_rev)-ord)])
             for ss in all_combos:
-                d[ss] += c1[ss] + c2[ss]
+                d[ss] += c1[ss] + c2[ss] if include_revComp else 0
     return d
 
 getBgCounts = getBgCounts_FAST_ACCURATE_BIGMEMORY
@@ -225,7 +236,7 @@ def getBgFreqs( bgCounts ):
     k = np.array(bgCounts.keys())
     nc = np.array([ len(i) for i in k ])
     d = {}
-    for i in range( nc.max() ):
+    for i in xrange( nc.max() ):
         ks = k[ nc == i+1 ]
         tot = sum( np.array([bgCounts[j] for j in ks]) )
         for j in ks:
