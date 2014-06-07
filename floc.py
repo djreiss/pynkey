3dd67af09d8a50fe613a205d0eb6d5d75a7e456c
David Reiss <dreiss@systemsbiology.org>
2014-06-06 17:52:00 -0700

    updated module dependencies

--- floc.py
+++ floc.py
@@ -2,22 +2,23 @@
 
 import pandas as pd
 
-import globals
 import utils as ut
 import scores
-from Bicluster import bicluster
+from Bicluster import bicluster,fill_all_cluster_scores_par
+
+print 'importing floc'
 
 ## Get gain scores for all possible row/col moves
 ## Default: allow best 5 row- and 20 col- moves per bicluster instead of all of them!
 def get_floc_scores_all(clusters, iter, all_genes, ratios, string_net, max_row=9999, max_col=9999):  ## 5, 20)
     ## First, collate move scores into a single DataFrame for stochastic sorting
-    ## Use DataFrames for scores rather than matrix
+    ## Use pd.DataFrame for scores rather than matrix
 
     ##bicluster.fill_all_cluster_scores(clusters, all_genes, ratios, string_net, ratios.columns.values)
-    clusters = globals.fill_all_cluster_scores_par(clusters, threads=15)
+    clusters = fill_all_cluster_scores_par(clusters, threads=15)
 
-    globals.counts_g = bicluster.get_all_cluster_row_counts( clusters, all_genes ) ## Clusters per gene counts - precompute
-    tmp = [ clusters[k].get_floc_scoresDF_rows(iter, all_genes, ratios, globals.counts_g) for k in clusters ]
+    counts_g = bicluster.get_all_cluster_row_counts( clusters, all_genes ) ## Clusters per gene counts - precompute
+    tmp = [ clusters[k].get_floc_scoresDF_rows(iter, all_genes, ratios, counts_g) for k in clusters ]
     scoresDF_r = pd.concat(tmp) ## This is much faster than mapreduce() above!!!
     del tmp
 
@@ -30,7 +31,7 @@ def get_floc_scores_all(clusters, iter, all_genes, ratios, string_net, max_row=9
     scoresDF_c = pd.concat(tmp) ## This is much faster than mapreduce()
     del tmp
 
-    weight_r, weight_n, weight_m, weight_c, weight_v, weight_g = scores.get_score_weights( iter, ratios )
+    ##weight_r, weight_n, weight_m, weight_c, weight_v, weight_g = scores.get_score_weights( iter, ratios )
 
     tmp1 = scores.get_combined_scores( scoresDF_c, iter, ratios )
     tmp1 = ut.sdize_vector( tmp1 ) * weight_c
@@ -58,13 +59,8 @@ def get_floc_scores_all(clusters, iter, all_genes, ratios, string_net, max_row=9
 #         scoresDF_c = rbind( shrunk_c )
 #     end
 
-#     scores = vcat(scoresDF_r, scoresDF_c)
-#     #println(scores)
-#     #write_table("output/$(organism)_scores.tsv",scores) 
-#     scores
-# end
-
-# #get_floc_scores_best( scores::DataFrame ) = get_floc_scores_best( scores, 3, 3 ) ## default n_best=2
+    scores = pd.concat([scoresDF_r, scoresDF_c])
+    return scores
 
 # ## Instead of scores for ALL possible moves, make a matrix of n_best best scores for each row/col
 # function get_floc_scores_best( scores::DataFrame, n_best_row::Int64=3, n_best_col::Int64=3 )
